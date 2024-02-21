86782238fcfa3723877b36db237a5e4a79720423
betalantz <betalantz@gmail.com>
2024-02-20 20:52:31 -0800

    completes lecture delivs and adds gitignore

--- 01-intro-to-flask/server/debug.py
+++ 01-intro-to-flask/server/debug.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
+import ipdb
 from app import app
-from models import db, Production
-#This debug file can be used in place of flask shell
-if __name__ == '__main__':
-    import ipdb; ipdb.set_trace()
+from models import Production, db
+
+# This debug file can be used in place of flask shell
+if __name__ == "__main__":
+    with app.app_context():
+
+        productions = Production.query.all()
+        p1 = Production.query.first()
+
+    ipdb.set_trace()
